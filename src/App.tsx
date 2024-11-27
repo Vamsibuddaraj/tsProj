714325340f24e900393c4794241cd11d2cd94738
Vamsibuddaraju <vamshikrishnamraju007@gmail.com>
2024-11-27 15:36:23 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return <div>hllo
+    <Button variant={'destructive'}>Subscribe</Button>
+  </div>
+}
+
+export default App
